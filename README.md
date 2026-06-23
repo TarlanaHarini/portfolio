baefa003849fb37667fceb663f92722cbbb54533
Tarlana Harini <tarlanaharini2003@gmail.com>
2026-06-23 21:21:20 +0530

    Portfolio updated

---
+++ README.md
@@ -0,0 +1,146 @@
+# Tarlana Harini Portfolio
+
+Welcome to my personal portfolio website. This portfolio showcases my skills, projects, education, internship experience, and achievements as a Flutter Developer, UI/UX Designer, and Information Technology graduate.
+
+## Live Portfolio
+
+Portfolio Link: Add your deployed Netlify link here
+
+## About Me
+
+I am Tarlana Harini, a B.Tech graduate in Information Technology from Aditya Institute of Technology and Management (AITAM). I am passionate about mobile application development, UI/UX design, and software development. I enjoy building user-friendly applications and continuously improving my technical skills.
+
+## Features
+
+* Personal Introduction
+* Technical Skills
+* Education Details
+* Internship Experience
+* Project Showcase
+* Resume Download
+* Contact Information
+* Responsive Design
+
+## Projects
+
+### Secure Mobile Voting System
+
+A secure mobile voting application developed using Flutter and MongoDB. The application includes OTP verification, fingerprint authentication, face recognition, and liveness detection to provide secure and reliable voting.
+
+### Mass Mail Dispatcher
+
+An email automation application that enables efficient bulk email delivery through a simple and user-friendly interface.
+
+### OIBSIP Projects
+
+A collection of web development projects completed during the Oasis Infobyte internship program.
+
+## Skills
+
+### Programming Languages
+
+* Java
+* Python
+* JavaScript
+* Dart
+
+### Frontend Technologies
+
+* HTML
+* CSS
+* JavaScript
+* Flutter
+
+### Databases
+
+* MongoDB
+* MySQL
+
+### Tools & Platforms
+
+* Figma
+* Git
+* GitHub
+* VS Code
+* Android Studio
+* Netlify
+
+## Internship Experience
+
+### UI/UX Intern
+
+**Qvalivon Technologies Pvt. Ltd.**
+
+* Designed high-fidelity wireframes and prototypes using Figma.
+* Created BRD and FRD documentation.
+* Collaborated with teams to improve user experience and interface design.
+
+## Education
+
+### B.Tech - Information Technology
+
+Aditya Institute of Technology and Management (AITAM)
+2021 - 2025
+
+### Intermediate
+
+Vihan Junior College
+2019 - 2021
+
+### SSC
+
+Narayana E-Techno School
+2018 - 2019
+
+## Technologies Used
+
+* React.js
+* JavaScript
+* HTML5
+* CSS3
+* Styled Components
+* GitHub
+* Netlify
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/TarlanaHarini/your-repository-name.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd your-repository-name
+```
+
+3. Install dependencies:
+
+```bash
+npm install
+```
+
+4. Start the application:
+
+```bash
+npm start
+```
+
+5. Open:
+
+```text
+http://localhost:3000
+```
+
+## Contact
+
+GitHub: https://github.com/TarlanaHarini
+
+LinkedIn: https://www.linkedin.com/in/harini-tarlana/
+
+## License
+
+This project is created for personal portfolio and professional showcase purposes.
+
---
+++ src/data/constants.js
@@ -0,0 +1,241 @@
+export const Bio = {
+  name: "Tarlana Harini",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "UI/UX Designer",
+    "B.Tech IT Graduate",
+  ],
+  description:
+    "To leverage my technical expertise in Information Technology and my passion for innovation to contribute meaningfully to organizational growth. I am committed to embracing new challenges, actively participating in diverse projects, and fostering a collaborative and forward-thinking work environment. Through continuous learning and professional development, I aim to align my aspirations with the goals of the organization, driving mutual success and long-term impact.",
+  github: "https://github.com/TarlanaHarini",
+  resume:
+    "https://drive.google.com/file/d/1HQRt9ukn7_pBPJ1-8XlkPqdFPiWwjzHu/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/harini-tarlana/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+  
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+ export const experiences = [
+
+{
+id: 0,
+img: "",
+role: "UI/UX Intern",
+company: "Qvalivon Technologies Pvt. Ltd.",
+date: "May 2024",
+desc: "Designed 15+ high-fidelity wireframes and UI layouts using Figma. Created BRD and FRD documents, collaborated with cross-functional teams, and contributed to user-centered design solutions.",
+skills: [
+"Figma",
+"UI/UX Design",
+"Wireframing",
+"Prototyping",
+"Documentation",
+"Team Collaboration",
+],
+doc: "",
+},
+
+{
+id: 1,
+img: "",
+role: "Flutter App Developer",
+company: "Academic Project",
+date: "2024 - 2025",
+desc: "Developed a Secure Mobile Voting System using Flutter and MongoDB. Implemented OTP verification, fingerprint authentication, face recognition, and liveness detection for secure voting.",
+skills: [
+"Flutter",
+"Dart",
+"MongoDB",
+"Face Recognition",
+"Figma",
+],
+doc: "",
+},
+
+{
+id: 2,
+img: "",
+role: "Java Full Stack Trainee",
+company: "Wipro TalentNext",
+date: "2024",
+desc: "Completed Java Full Stack Professional Training Program.",
+skills: [
+"Java",
+"HTML",
+"CSS",
+"SQL",
+"GitHub",
+],
+doc: "",
+},
+ ];
+
+ export const education = [
+{
+id: 0,
+img: "",
+school: "Aditya Institute of Technology and Management (AITAM), Tekkali",
+date: "2021 - 2025",
+grade: "B.Tech",
+desc: "Bachelor of Technology in Information Technology.",
+degree: "B.Tech - Information Technology",
+},
+{
+id: 1,
+img: "",
+school: "Vihan Junior College, Visakhapatnam",
+date: "2019 - 2021",
+grade: "73%",
+desc: "Completed Intermediate education.",
+degree: "Intermediate",
+},
+{
+id: 2,
+img: "",
+school: "Narayana E-Techno School, Palasa",
+date: "2018 - 2019",
+grade: "8.7 GPA",
+desc: "Completed SSC.",
+degree: "SSC",
+},
+];
+export const projects = [
+{
+id: 1,
+title: "Secure Mobile Voting System",
+date: "2025",
+description:
+"Developed a secure mobile voting application using Flutter and MongoDB with OTP verification, face recognition, fingerprint authentication, and liveness detection.",
+image: require("../images/voting.png"),
+tags: ["Flutter", "Dart", "MongoDB", "Figma"],
+category: "android app",
+github: "https://github.com/TarlanaHarini",
+},
+
+{
+id: 2,
+title: "Mass Mail Dispatcher",
+date: "2025",
+description:
+"Developed a bulk email dispatch system that automates sending emails to multiple recipients efficiently.",
+image: require("../images/mail.jpg"),
+tags: ["Java", "HTML", "CSS", "JavaScript"],
+category: "web app",
+github: "https://github.com/TarlanaHarini/Mass--mail-dispatcher",
+},
+
+{
+id: 3,
+title: "OIBSIP Projects",
+date: "2024",
+description:
+"Completed multiple web development projects during Oasis Infobyte internship, demonstrating front-end development skills.",
+image: require("../images/oibsip.jpg"),
+tags: ["HTML", "CSS", "JavaScript", "Java"],
+category: "web app",
+github: "https://github.com/TarlanaHarini/OIBSIP",
+},
+];
+
+export const TimeLineData = [
+{ year: 2021, text: "Started B.Tech in Information Technology at AITAM" },
+{ year: 2024, text: "Completed UI/UX Internship at Qvalivon Technologies" },
+{ year: 2024, text: "Completed Wipro TalentNext Java Full Stack Training" },
+{ year: 2025, text: "Developed Secure Mobile Voting System using Flutter" },
+{ year: 2025, text: "Graduated with B.Tech in Information Technology" },
+];
+
